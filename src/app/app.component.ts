24ce2273f897d7dff59b42e2f336f12d827cb50d
gbelenky <gbelenky@microsoft.com>
2022-06-16 17:04:03 +0200

    going back to the initial version

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,8 +2,38 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  template: `<div>Hello {{value}}</div>`,
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  value = 'World';
+
+  data: {name: string, series: { name: string, value: number }[] }[];
+  barColor = ['#a9ce97', '#a5b5de'];
+  domain = [100, 1000];
+
+  constructor() {
+    this.data = [
+      {
+        name: 'Row1',
+        series: [
+          {name: 'Bar1', value: 150},
+          {name: 'Bar2', value: 200}
+        ],
+      },
+      {
+        name: 'Row2',
+        series: [
+          {name: 'Bar1', value: 300},
+          {name: 'Bar2', value: 400}
+        ],
+      },
+      {
+        name: 'Row3',
+        series: [
+          {name: 'Bar1', value: 500},
+          {name: 'Bar2', value: 1000}
+        ],
+      }
+    ];
+  }
 }
